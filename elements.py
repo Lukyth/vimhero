9d20d74bbf462be9d295fed96fc36ea0f06e4694
Lukyth <k.sujautra@gmail.com>
2014-12-15 14:34:07 +0700

    random direction when start

--- elements.py
+++ elements.py
@@ -1,13 +1,25 @@
 import pygame
 from pygame.locals import *
+from random import randint
 
 class Arrow(object):
+
+    DIRECTION = ['up', 'down', 'left', 'right']
+
     def __init__(self, pos):
         (self.x, self.y) = pos
-        self.image = pygame.image.load("img/up.png")
-        self.image.get_rect()
-        
+        self.direction = self.random_direction()
+        self.image = self.get_image(self.direction)
 
     def render(self, surface):
         pos = (int(self.x), int(self.y))
         surface.blit(self.image, pos)
+
+    def random_direction(self):
+        return self.DIRECTION[randint(0, len(self.DIRECTION) - 1)]
+
+    def get_image(self, direction):
+        return pygame.image.load("img/" + direction + ".png")
+
+    def get_direction(self):
+        return self.direction
